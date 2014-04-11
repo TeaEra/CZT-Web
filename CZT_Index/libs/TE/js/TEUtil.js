9644c191fab02367c83dc9a713f4fdd5a37471ac
CHEN Zhengong <cllf08214@qq.com>
2014-04-11 00:37:06 +0000

    Remove bootstrap 2.x;

---
+++ CZT_Index/libs/TE/js/TEUtil.js
@@ -0,0 +1,15 @@
+(function () {
+  "use strict";
+  
+  // some codes here;
+  function test() {
+    alert(123);
+  }
+  
+  window.teaera = window.teaera || {};
+  window.teaera.cv = {
+    "name": "Zhengtong CHEN",
+    "name_en": "Vince",
+    "name_zh": "陈政通"
+  };
+})();

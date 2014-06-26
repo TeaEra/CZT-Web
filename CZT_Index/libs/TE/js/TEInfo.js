6a93c8b6c937afea2ab925d2370dd750824a6c6c
TeaEra <cllf08214@qq.com>
2014-06-26 15:01:13 +0800

    Keep refactoring and little improvements;

---
+++ CZT_Index/libs/TE/js/TEInfo.js
@@ -0,0 +1,15 @@
+(function () {
+    "use strict";
+
+    // some codes here;
+    function helloTE() {
+        alert("Hello, TeaEra!");
+    }
+
+    window.teaera = window.teaera || {};
+    window.teaera.info = {
+        "name": "Zhengtong CHEN",
+        "name_en": "Vince",
+        "name_zh": "陈政通"
+    };
+})();
---
+++ CZT_Index/libs/TE/js/te-util.js
@@ -0,0 +1,5 @@
+/**
+ * Created by TeaEra on 2014/6/26.
+ */
+
+"use strict";

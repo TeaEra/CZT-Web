3aab1eb5d5654868c591c75d563b306864548024
TeaEra <cllf08214@qq.com>
2014-08-30 13:52:37 +0800

    Refactor file structure and rename;

--- CZT_Index/libs/TE/js/TEInfo.js
+++ CZT_Index/libs/TE/js/TEInfo.js
@@ -14,37 +14,43 @@
         "name_zh" : "陈政通"
     };
 
-    //##################################################################################################################
+    //##########################################################################
     window.teaera.test = [
         {
             "title" : "Black&White index",
-            "name" : "test_index_b&w.html",
-            "url" : "libs/TE/test/pages/test_index_b&w.html"
+            "name" : "test-index-grayscale.html",
+            "url" : "libs/TE/test/" +
+                "test-index-grayscale/test-index-grayscale.html"
         },
         {
             "title" : "Persona",
-            "name" : "test_persona.html",
-            "url" : "libs/TE/test/pages/test_persona.html"
+            "name" : "test-persona.html",
+            "url" : "libs/TE/test/" +
+                "test-persona/test-persona.html"
         },
         {
             "title" : "TimeLine",
-            "name" : "test_timeline.html",
-            "url" : "libs/TE/test/pages/test_timeline.html"
+            "name" : "test-timeline.html",
+            "url" : "libs/TE/test/" +
+                "test-timeline/test-timeline.html"
         },
         {
             "title" : "FontAwesome",
-            "name" : "test_fontawesome.html",
-            "url" : "libs/TE/test/pages/test_fontawesome.html"
+            "name" : "test-fontawesome.html",
+            "url" : "libs/TE/test/" +
+                "test-fontawesome/test-fontawesome.html"
         },
         {
             "title" : "Demo for Penny",
-            "name" : "test_forPenny.html",
-            "url" : "libs/TE/test/pages/test_forPenny.html"
+            "name" : "test-for-Penny.html",
+            "url" : "libs/TE/test/" +
+                "test-for-Penny/test-for-Penny.html"
         },
         {
             "title" : "A keyboard",
-            "name" : "test_keyboard.html",
-            "url" : "libs/TE/test/pages/test_keyboard.html"
+            "name" : "test-keyboard.html",
+            "url" : "libs/TE/test/" +
+                "test-keyboard/test-keyboard.html"
         }
     ];
 })();

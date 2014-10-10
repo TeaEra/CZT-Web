108825b6967b14cc2fe1d155dd6c1a535d843e2d
CHEN Zhengong <cllf08214@qq.com>
2014-10-10 18:25:39 +0800

    Keep succeeding on extension developing;

--- CZT_JS/TE-Extensions/helloext/webkit-start.js
+++ CZT_JS/TE-Extensions/helloext/webkit-start.js
@@ -9,7 +9,11 @@
         console.log("No localStorage;");
     }
 
-    var bottom_div = document.createElement("div");
+    /*var bottom_div = document.createElement("div");
     bottom_div.innerHTML = "<h3>testing..............................</h3>";
-    document.body.appendChild(bottom_div);
+    document.body.appendChild(bottom_div);*/
+
+    window.show_welcome = function () {
+        alert("Welcome!");
+    };
 })();

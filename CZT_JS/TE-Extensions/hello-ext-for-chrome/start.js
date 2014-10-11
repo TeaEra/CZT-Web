d5ec73efc6a82e502d3e668110f168796c23802a
CHEN Zhengong <cllf08214@qq.com>
2014-10-11 16:08:01 +0800

    Keep succeeding on extension developing - for Chrome;

---
+++ CZT_JS/TE-Extensions/hello-ext-for-chrome/start.js
@@ -0,0 +1,7 @@
+/**
+ * Created by chenzhengtong on 2014/10/11.
+ */
+
+var bottom_div = document.createElement("div");
+bottom_div.innerHTML = "<h3>testing...</h3>";
+document.body.appendChild(bottom_div);

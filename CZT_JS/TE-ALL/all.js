768af2bd52709412b80fc60a9d0fa6880722475e
CHEN Zhengong <cllf08214@qq.com>
2014-10-11 13:43:16 +0800

    check ie version;

--- CZT_JS/TE-ALL/all.js
+++ CZT_JS/TE-ALL/all.js
@@ -9,14 +9,19 @@
     window.all = window.all || {};
 
     /**
-     * Link: http://bbs.html5cn.org/thread-80269-1-1.html
+     * [Link](http://bbs.html5cn.org/thread-80269-1-1.html)
      *
      * from Mozilla Aurora 11;
      * only 'window.navigator.battery' available for now;
+     *
      */
     window.all.test_battery = function () {
         // 获取电池对象!
         var battery = navigator.battery || navigator.webkitBattery || navigator.mozBattery;
+
+        if (! battery) {
+            return;
+        }
 
         // 显示一些有用属性值
         console.warn("电池充电状态: ", battery.charging); // true
@@ -36,6 +41,28 @@
         battery.addEventListener("levelchange", function(e) {
             console.warn("电量水平变化: ", battery.level);
         }, false);
+    };
+
+    /**
+     * [Link](https://gist.github.com/padolsey/527683)
+     *
+     */
+    window.all.get_IE_version = function () {
+        // for-loop saves characters over while
+        for (
+            var v = 3,
+            // b just as good as a div with 2 fewer characters
+            el = document.createElement('b'),
+            // el.all instead of el.getElementsByTagName('i')
+            // empty array as loop breaker (and exception-avoider) for non-IE and IE10+
+            all = el.all || [];
+            // i tag not well-formed since we know that IE5-IE9 won't mind
+            el.innerHTML = '<!--[if gt IE ' + (++v) + ']><i><![endif]-->',
+            all[0];
+        );
+        // instead of undefined, returns the documentMode for IE10+ compatibility
+        // non-IE will still get undefined as before
+        return v > 4 ? v : document.documentMode;
     };
 
 })();

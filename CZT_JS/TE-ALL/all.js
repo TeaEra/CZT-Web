c193df235bab1b67c6c8560885e8b23f4b54148c
CHEN Zhengong <cllf08214@qq.com>
2014-12-15 11:20:26 +0800

    整理文件； new template files；

--- CZT_JS/TE-ALL/all.js
+++ CZT_JS/TE-ALL/all.js
@@ -65,4 +65,22 @@
         return v > 4 ? v : document.documentMode;
     };
 
+    /**
+     * Created by TeaEra on 2014-08-06.
+     */
+    window.all.get_xhr = function () {
+        var xhr;
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        }
+        else if(window.ActionXObject) {
+            xhr = new ActiveXObject("Msxml2.XMLHTTP")
+                | new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        else {
+            throw new Error("xhr is not supported");
+        }
+        return xhr;
+    };
+
 })();
